24b868686eac84a8cb4401afb429004fa00d8950
Atulmahajan12345 <atulmahajan2299@gmail.com>
2022-03-25 14:03:31 +0530

    evenodd program

---
+++ src/day5/EvenOdd.java
@@ -0,0 +1,27 @@
+package day5;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		int a,b;
+		
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter any number");
+		a=s.nextInt();
+		
+		if(a%2==0)
+			System.out.println("Even number");
+		else if (a%2==1)
+			System.out.println("Odd number");
+	
+	
+	
+
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
